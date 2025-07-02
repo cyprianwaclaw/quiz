dc27f3f2d557b4bd1c55170a12dc286def792b8a
Cyprian Wacław <cyprianwaclaw@gmail.com>
2025-07-02 14:24:14 +0200

    fvgfg

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -464,26 +464,26 @@ class PaymentController extends APIController
 
         // Pobranie płatności
         // $payments = Payment::where("subscriber_id", $user->id)
-        $payments = Payment::where("user_id", $user->id)->paginate($perPage, ['*'], 'page', $page);
-            // ->select(
-            //     'payments.id as payment_id',
-            //     'payments.status',
-            //     'payments.error_code',
-            //     'payments.error_description',
-            //     'payments.session_id',
-            //     // 'payments.plan_subscription_id',
-            //     'payments.ifirma_invoice_id',
-            //     'payments.created_at',
-            //     'payments.updated_at'
-            // )
-            // ->with(['planSubscription' => function ($query) {
-            //     $query->select('id as plan_subscription_id', 'subscriber_id as laravel_through_key');
-            // }])
-            // ->whereHas('planSubscription', function ($query) use ($user) {
-            //     $query->where('subscriber_id', $user->id)->whereNull('deleted_at');
-            // })
+        $payments = Payment::where("user_id", $user->id)
+            ->select(
+                'payments.id as payment_id',
+                'payments.status',
+                'payments.error_code',
+                'payments.error_description',
+                'payments.session_id',
+                // 'payments.plan_subscription_id',
+                'payments.ifirma_invoice_id',
+                'payments.created_at',
+                'payments.updated_at'
+            )
+            ->with(['planSubscription' => function ($query) {
+                $query->select('id as plan_subscription_id', 'subscriber_id as laravel_through_key');
+            }])
+            ->whereHas('planSubscription', function ($query) use ($user) {
+                $query->where('subscriber_id', $user->id)->whereNull('deleted_at');
+            })
             // ->orderBy('payments.created_at', 'asc')
-            // ->paginate($perPage, ['*'], 'page', $page);
+            ->paginate($perPage, ['*'], 'page', $page);
 
         // // Konwersja kolekcji na zmodyfikowane dane przed przypisaniem do paginacji
         // $modifiedPayments = $payments->getCollection()->map(function ($payment) {
