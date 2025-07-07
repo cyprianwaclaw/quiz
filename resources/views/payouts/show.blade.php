231f0168f78d26a32b123319323c14cfe5697359
Cyprian Wacław <cyprianwaclaw@gmail.com>
2025-07-07 21:15:05 +0200

    test v1

--- resources/views/payouts/show.blade.php
+++ resources/views/payouts/show.blade.php
@@ -51,8 +51,8 @@
                                     @foreach($statuses as $key=>$status)
                                     <option value="{{$key}}" @if($key == $payout->status) selected @endif >{{$status}}</option>
                                     @endforeach
-                                </select>
-                                <button class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800" type="submit">Akceptuj</button> --}}
+                                </select> --}}
+                                <button class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800" type="submit">Akceptuj</button>
                             </form>
                         </div>
                     </div>
